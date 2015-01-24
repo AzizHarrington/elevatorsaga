688f9d82cef1208826eca82bf751fd62d5ab5452
azizharrington <azizharrington@gmail.com>
2015-01-24 14:48:50 -0600

    challenge 4 real solution

--- solutions.js
+++ solutions.js
@@ -1,57 +1,40 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+        Array.prototype.max = function() {
+          return Math.max.apply(null, this);
+        };
 
-        startElevator(elevator);
-
-        function startElevator(elevator) {
-            elevator.on("idle", function() {
-                forEach(floors, function (floor) {
-                    checkForPassenger(floor, elevator);
-                });
-                checkFloorButton(elevator);
+        floors.forEach(function (floor) {
+            floor.on("up_button_pressed down_button_pressed", function() {
+                sendElevator(floor);
             });
-            checkPassingFloors(elevator);
-        }
+        });
 
-        function checkPassingFloors(elevator) {
-            elevator.on("passing_floor", function(floorNum, direction) {
-                decideToStop(elevator, floorNum);
-            });
-        }
-
-        function decideToStop(elevator, floorNum) {
-            var floorIndex = elevator.destinationQueue.indexOf(floorNum);
-            console.log("checking queue for floor " + floorNum);
-            console.log(elevator.destinationQueue);
-            if (floorIndex > -1) {
-                elevator.destinationQueue.splice(floorIndex, 1);
-                elevator.destinationQueue.unshift(floorIndex);
-                elevator.checkDestinationQueue();
-                console.log("stopping at " + floorNum);
-                console.log(elevator.destinationQueue);
-            }
-        }
+        elevators.forEach(function (elevator) {
+            checkFloorButton(elevator);
+        });
 
         function checkFloorButton(elevator) {
             elevator.on("floor_button_pressed", function(floorNum) {
                 if (elevator.destinationQueue.indexOf(floorNum) === -1) {
                     elevator.goToFloor(floorNum);
                 }
-            })
-        }
-
-        function checkForPassenger(floor, elevator) {
-            floor.on("up_button_pressed down_button_pressed", function () {
-                if (elevator.destinationQueue.indexOf(floor.floorNum()) === -1) {
-                    elevator.goToFloor(floor.floorNum());
-                }
             });
         }
 
-        function forEach(arr, func) {
-            for (var i = 0; i < arr.length; i++) {
-                func(arr[i]);
+        function sendElevator(floor) {
+            var floorNo = floor.floorNum();
+            elevators.forEach(function (elevator) {
+                if (elevator.destinationQueue.indexOf(floorNo) === -1 && floorNo != null) {
+                    elevator.goToFloor(floorNo);
+                    floorNo = null;
+                    rotateElevators(elevator);
+                }
+            });
+            function rotateElevators(elevator) {
+                var index = elevators.indexOf(elevator);
+                elevators.splice(index, 1);
+                elevators.push(elevator);
             }
         }
     },
